0b068abf26a512d77b1b05fcc074258e83abe91f
skatsampos <R!3hdyfp>
2021-11-02 15:20:05 +0200

    solve foreign key problem

--- src/main/java/com/site/woolencreations/user/UserConfig.java
+++ src/main/java/com/site/woolencreations/user/UserConfig.java
@@ -29,10 +29,20 @@ public class UserConfig {
                     .city("Agia Paraskevi")
                     .country("Greece")
                     .build();
+            Address address2 = Address
+                    .builder()
+                    .address("lightship way")
+                    .number(9)
+                    .postCode("CO2 8GY")
+                    .city("Colchester")
+                    .country("UK")
+                    .build();
 
             List<Address> addresslist = new ArrayList();
+            List<Address> addresslist1 = new ArrayList();
             addresslist.add(address);
             addresslist.add(address1);
+            addresslist1.add(address2);
 
             User user = User
                     .builder()
@@ -46,7 +56,20 @@ public class UserConfig {
                     .role("customer")
                     .phone("6900000000")
                     .build();
+            User user1 = User
+                    .builder()
+                    .username("xxx@gmail.com")
+                    .first_name("spi")
+                    .last_name("kat")
+                    .password("aaa")
+                    .addresses(addresslist1)
+                    .points(150)
+                    .guest(false)
+                    .role("customer")
+                    .phone("6900000001")
+                    .build();
             repository.save(user);
+            repository.save(user1);
         };
     }
 }

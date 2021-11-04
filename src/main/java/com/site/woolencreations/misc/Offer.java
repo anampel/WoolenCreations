d7a8fb3f7d8f90f02ed3f6b043d8721d7bee1d5d
skatsampos <R!3hdyfp>
2021-11-04 16:53:02 +0200

    add offer

---
+++ src/main/java/com/site/woolencreations/misc/Offer.java
@@ -0,0 +1,39 @@
+package com.site.woolencreations.misc;
+
+import com.site.woolencreations.product.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "OFFER")
+@Entity
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Offer {
+    @Id
+    @SequenceGenerator(
+            name = "offer_sequence",
+            sequenceName = "offer_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "offer_sequence"
+    )
+    private int offerId;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name="product_id")
+    private List<Product> productList;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+    private double discount;
+}
--- src/main/java/com/site/woolencreations/product/Product.java
+++ src/main/java/com/site/woolencreations/product/Product.java
@@ -1,6 +1,7 @@
 package com.site.woolencreations.product;
 
 import com.site.woolencreations.misc.Category;
+import com.site.woolencreations.misc.Offer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,12 +26,18 @@ public class Product {
     private String name;
     private String description;
     private Double price;
+
     @ManyToMany(cascade = CascadeType.ALL )
     @JoinTable(
             name = "product_category",
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "categoryId"))
     private List<Category> categoryList;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="offer_id")
+    private Offer offer;
+
     private int points;
     private Double discount;
 
